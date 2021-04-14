db24a234b2bddd6b540d8ff236fd96e62e365395
brunocesarti <opusdey@132015>
2021-04-13 21:44:56 -0300

    Importando o modulo da toolbar do material design #4

--- challenge/src/app/app.module.ts
+++ challenge/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 
+import {MatToolbarModule} from "@angular/material/toolbar";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { HeaderComponent } from './components/template/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
