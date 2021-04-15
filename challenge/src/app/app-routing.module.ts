15979b07e81272deb98036ccdd37114ae479b30d
brunocesarti <opusdey@132015>
2021-04-14 22:11:01 -0300

    Adicionando rotas de navegação #4

--- challenge/src/app/app-routing.module.ts
+++ challenge/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./views/home/home.component";
+import { FormularioComponent } from "./views/formulario/formulario.component";
+
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent
+}, {
+  path: "formulario",
+  component: FormularioComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
