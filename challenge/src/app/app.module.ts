4eb827a5e0364759ff7e0460ade550b5a6f88c54
brunocesarti <opusdey@132015>
2021-04-14 21:44:34 -0300

    Importando angular-animations no app.module #4

--- challenge/src/app/app.module.ts
+++ challenge/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule} from "@angular/material/toolbar";
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
@@ -24,6 +25,7 @@ import { HomeComponent } from './views/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
     MatListModule
